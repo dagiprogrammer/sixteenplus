ac5f2d3dab87898d19b25320aa38196a5d08d3fa
dagiprogrammer <dagiprogrammer@gmail.com>
2025-12-29 00:57:24 +0300

    fixed 1

--- sixteen-plus-website/src/App.tsx
+++ sixteen-plus-website/src/App.tsx
@@ -3,9 +3,9 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Header from './components/Header';
 import Home from './pages/Home';
-import About from './pages/About';
-import Products from './pages/Products';
-import Contact from './pages/Contact';
+// import About from './pages/About';
+// import Products from './pages/Products';
+// import Contact from './pages/Contact';
 import Footer from './components/Footer';
 
 function App() {
@@ -17,9 +17,9 @@ function App() {
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} /> */}
         </Routes>
       </AnimatePresence>
       <Footer />
